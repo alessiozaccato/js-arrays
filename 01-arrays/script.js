38c669b20cfe9f6976545f35544872210bf06ca0
alessiozaccato <alessiozaccato@gmail.com>
2025-01-13 12:59:27 +0100

    SOLVE: script base section

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,24 +10,46 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = null;
+const fourthTeacher = teachers[3];
+console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+teachers.splice(4, 1, "Patrick");
+console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+console.log(lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+console.log(firstTeacher);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push("Vanessa");
+console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift("Sarah");
+console.log(teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = teachers.indexOf("Lewis");
+console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+//FIRST WAY
+const isTeachersEmpty = teachers.length !== 0; //using negation of the boolean value
+console.log(isTeachersEmpty);
+
+//SECOND WAY
+let isTeachersEmpty1 = null;// i need to declare a different name here, let and not const cause i want to edit this value
+if (teachers.length == 0) {
+  isTeachersEmpty1 = true;
+} else {
+  isTeachersEmpty1 = false;
+}
+
+console.log(isTeachersEmpty1);
